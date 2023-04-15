aebc4b6d1e04a0cff057aaae89f48ab9d16d8f88
Mushfique Azam <123544326+Musfik41@users.noreply.github.com>
2023-04-16 01:03:21 +0530

    ino_update_v2
    Added 4 different led indicator for visuals with no errors.

--- GPIO-ESP32/GPIO-ESP32.ino
+++ GPIO-ESP32/GPIO-ESP32.ino
@@ -1,4 +1,4 @@
-//#include "BlinkTimes.h"
+#include "BlinkTimes.h"
 
 // define pins for LED and push buttons
 const int LED_PIN_2 = 2;
@@ -6,19 +6,17 @@ const int BUTTON_PIN_21 = 21;
 const int BUTTON_PIN_22 = 22;
 const int BUTTON_PIN_23 = 23;
 
-
-unsigned long blinkTimes[4] = {
-  1,   // On Sequence 1 (in second)
-  11,   // Off Sequence 2 (in second)
-  
-  15,  // On Sequence 3 (in second)
-  1,  // Off Sequence 4 (in second)
-};
+const int LED_PIN_15 = 15;
+const int LED_PIN_4 = 4;
+const int LED_PIN_5 = 5;
+const int LED_PIN_19 = 19;
 
 
 // define variables for LED state and blink interval
 bool ledOn = false;
 bool paused = false;
+int lastButton = 0;
+
 
 
 unsigned long lastBlinkTime = 0;
@@ -34,49 +32,96 @@ void setup() {
   pinMode(BUTTON_PIN_22, INPUT_PULLUP);
   pinMode(BUTTON_PIN_23, INPUT_PULLUP);
 
+  pinMode(LED_PIN_15, OUTPUT);
+  pinMode(LED_PIN_4, OUTPUT);
+  pinMode(LED_PIN_5, OUTPUT);
+  pinMode(LED_PIN_19, OUTPUT);
+
+
+
   // start serial communication
   Serial.begin(9600);
 }
 
 void loop() {
+          lastBlinkTime = millis(); // initialize last blink time to current time
+
   static unsigned long elapsedTime = 0;
 
   while (true) {
    
     // check if button 21 is pressed
     if (digitalRead(BUTTON_PIN_21) == LOW) {
-      ledOn = true; // turn on LED
-      paused = false; // resume blinking if previously paused
-      blinkIndex = 0; // reset blink index to start from beginning of array
-      digitalWrite(LED_PIN_2, HIGH); // turn on LED
-      Serial.println("Button 21 pressed"); // output message to serial monitor
-      delay(1000); // debounce delay
-      elapsedTime = 0; // reset elapsed time
+        lastButton = 21;
+        ledOn = true;
+        paused = false; // resume blinking if previously paused
+        blinkIndex = 0; // reset blink index to start from beginning of array
+        digitalWrite(LED_PIN_2, HIGH); // turn on LED
+        digitalWrite(LED_PIN_19, HIGH); // turn on LED19
+        Serial.println("Button 21 pressed"); // output message to serial monitor
+        delay(1000); // debounce delay
+        elapsedTime = 0; // reset elapsed time
+        lastBlinkTime = millis(); // initialize last blink time to current time
     }
 
     // check if button 22 is pressed
     if (digitalRead(BUTTON_PIN_22) == LOW) {
-      ledOn = false; // turn off LED
-      paused = true; // pause blinking
-      digitalWrite(LED_PIN_2, LOW); // turn off LED
-      Serial.println("Button 22 pressed"); // output message to serial monitor
-      Serial.println("LED Stopped"); // output message to serial monitor
-      delay(1000); // debounce delay
-      elapsedTime = 0; // reset elapsed time
+        lastButton = 22;
+        ledOn = false; // turn off LED
+        paused = true; // pause blinking
+        digitalWrite(LED_PIN_2, LOW); // turn off LED
+        digitalWrite(LED_PIN_19, LOW); // turn off LED19
+        Serial.println("Button 22 pressed"); // output message to serial monitor
+        Serial.println("LED Stopped"); // output message to serial monitor
+        delay(1000); // debounce delay
+        elapsedTime = 0; // reset elapsed time
     }
 
     // check if button 23 is pressed
     if (digitalRead(BUTTON_PIN_23) == LOW) {
+      lastButton = 23;
+      digitalWrite(LED_PIN_19, HIGH); // turn on LED19
       paused = !paused; // toggle pause state
       if (paused) {
         Serial.println("LED paused"); // output message to serial monitor
         elapsedTime = (millis() - lastBlinkTime) / 1000; // calculate elapsed time
       } else {
+                lastButton = 233;
+
         Serial.println("LED resumed"); // output message to serial monitor
         lastBlinkTime = millis() - (elapsedTime * 1000); // subtract elapsed time from current time
       }
       delay(1000); // debounce delay
     }
+
+    switch (lastButton) {
+  case 21:
+    digitalWrite(LED_PIN_15, ledOn);
+    digitalWrite(LED_PIN_4, LOW);
+    digitalWrite(LED_PIN_5, LOW);
+    break;
+  case 22:
+    digitalWrite(LED_PIN_15, LOW);
+    digitalWrite(LED_PIN_4, HIGH); // turn on LED_PIN_4
+    digitalWrite(LED_PIN_5, LOW);
+    break;
+  case 23:
+    digitalWrite(LED_PIN_15, LOW);
+    digitalWrite(LED_PIN_4, LOW);
+    digitalWrite(LED_PIN_5, HIGH);
+    break;
+  case 233:
+    digitalWrite(LED_PIN_15, ledOn);
+    digitalWrite(LED_PIN_4, LOW); // turn on LED_PIN_4
+    digitalWrite(LED_PIN_5, LOW);
+    break;
+  default:
+    digitalWrite(LED_PIN_15, ledOn);
+    digitalWrite(LED_PIN_4, LOW);
+    digitalWrite(LED_PIN_5, LOW);
+    break;
+}
+
 
     // blink LED according to on/off times if not paused
     if (!paused && (millis() - lastBlinkTime) >= (blinkTimes[blinkIndex] * 1000)) {
@@ -90,7 +135,7 @@ void loop() {
       Serial.println(blinkTimes[blinkIndex]);
       elapsedTime = 0; // reset elapsed time
       blinkIndex++; // increment blink index to move to next blink time
-      if (blinkIndex >= 4) {
+      if (blinkIndex >= 24) {
         blinkIndex = 0; // reset blink index to start from beginning of array
       }
     }
