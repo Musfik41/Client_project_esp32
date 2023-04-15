3a425da3d4f38fd1a7a0416cf02aabb5b95b1715
Mushfique Azam <123544326+Musfik41@users.noreply.github.com>
2023-04-16 01:00:53 +0530

    uncomments blinktimes.h
    Accept input values in minutes.

--- GPIO-ESP32/BlinkTimes.h
+++ GPIO-ESP32/BlinkTimes.h
@@ -1,43 +1,49 @@
-//#ifndef BLINK_TIMES_H
-//#define BLINK_TIMES_H
-//
-//
-//extern unsigned long blinkTimes[24] = {
-//  1,   // On Sequence 1 (in second)
-//  1,   // Off Sequence 2 (in second)
-//  
-//  2,  // On Sequence 3 (in second)
-//  1,  // Off Sequence 4 (in second)
-//  
-//  5,   // On Sequence 5 (in second)
-//  4,   // Off Sequence 6 (in second)
-//  
-//  2,   // On Sequence 7 (in second)
-//  3,   // Off Sequence 8 (in second)
-//  
-//  5,   // On Sequence 9 (in second)
-//  5,    // Off Sequence 10 (in second)
-//  
-//  1,   // On Sequence 11 (in second)
-//  1,   // Off Sequence 12 (in second)
-//  
-//  11,  // On Sequence 13 (in second)
-//  15,  // Off Sequence 14 (in second)
-//  
-//  3,   // On Sequence 15 (in second)
-//  3,   // Off Sequence 16 (in second)
-//  
-//  4,   // On Sequence 17 (in second)
-//  4,   // Off Sequence 18 (in second)
-//  
-//  5,   // On Sequence 19 (in second)
-//  5,    // Off Sequence 20 (in second)
-//  
-//  4,   // On Sequence 21 (in second)
-//  4,   // Off Sequence 22 (in second)
-//  
-//  1,   // On Sequence 23 (in second)
-//  5    // Off Sequence 24 (in second)
-//};
-//
-//#endif
+#ifndef BLINK_TIMES_H
+#define BLINK_TIMES_H
+
+const unsigned long SECONDS = 1;
+const unsigned long times = SECONDS * 60;
+
+//################################################### All values are stored in Minutes ##############################################################
+
+extern unsigned long blinkTimes[24] = {
+  1 * times,   // On Sequence 1 (in minutes)
+  1 * times,   // Off Sequence 2 (in minutes)
+  
+  2 * times,   // On Sequence 3 (in minutes)
+  1 * times,   // Off Sequence 4 (in minutes)
+  
+  1 * times,   // On Sequence 5 (in minutes)
+  1 * times,   // Off Sequence 6 (in minutes)
+  
+  2 * times,   // On Sequence 7 (in minutes)
+  1 * times,   // Off Sequence 8 (in minutes)
+  
+  1 * times,   // On Sequence 9 (in minutes)
+  1 * times,   // Off Sequence 10 (in minutes)
+  
+  2 * times,   // On Sequence 11 (in minutes)
+  1 * times,   // Off Sequence 12 (in minutes)
+  
+  1 * times,   // On Sequence 13 (in minutes)
+  1 * times,   // Off Sequence 14 (in minutes)
+  
+  2 * times,   // On Sequence 15 (in minutes)
+  1 * times,   // Off Sequence 16 (in minutes)
+  
+  1 * times,   // On Sequence 17 (in minutes)
+  1 * times,   // Off Sequence 18 (in minutes)
+  
+  2 * times,   // On Sequence 19 (in minutes)
+  1 * times,   // Off Sequence 20 (in minutes)
+  
+  1 * times,   // On Sequence 21 (in minutes)
+  1 * times,   // Off Sequence 22 (in minutes)
+  
+  2 * times,   // On Sequence 23 (in minutes)
+  1 * times,   // Off Sequence 24 (in minutes)
+
+  
+};
+
+#endif
